0ad5aacd856dc8fd03b2f137d65dabc92fd77a97
Madhusakth <madhu14mitha@gmail.com>
2018-10-30 08:21:55 -0600

    Add files via upload

---
+++ radio_receive.py
@@ -0,0 +1,16 @@
+from microbit import *
+import radio
+
+radio.on()
+radio.config(channel=19)        # Choose your own channel number
+radio.config(power=7)           # Turn the signal up to full strength
+
+#my_message = "Be nice to yu turkeys dis christmas, Cos' turkeys just wanna hav fun, Turkeys are cool, turkeys are wicked, An every turkey has a Mum."
+
+# Event loop.
+while True:
+        incoming = radio.receive()
+        if incoming is not None:
+            display.show(incoming)
+            print(incoming)
+        sleep(500)
