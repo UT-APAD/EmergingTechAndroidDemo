ef0258591319fd335b819e3c134329d269c60b0b
XUAN WANG <56691424+xxuanwang@users.noreply.github.com>
2020-11-20 15:03:02 -0600

    Update the file to work with step.csv

--- count.py
+++ count.py
@@ -1,29 +1,24 @@
 import numpy as np
 import pandas as pd
-#define X size 
-X = np.zeros((26*23,3))  #26 days data, 23 unique user id steps
-wea = pd.read_csv('../weather.csv')
-wea=np.array(wea)
-print (wea.shape)
-#Accumulate only the weather data from the array
-wea=wea[1:27,1:4]   #26 days data, column: high temp, low temp, humidity
-wea = np.asfarray(wea, float)
-print (wea.shape)
-#replicate weather for all the steps data
-for x in range(23):
-    X[26*x:26*(x+1),:]=wea   
-ones_x = np.ones((26*23,1))
+from sklearn.preprocessing import StandardScaler
 
-#create the X_matrix with Ones in the first column followed by weather data in the next
-X_ = np.concatenate((ones_x,X),axis=1)
+df = pd.read_csv(r"step.csv")
 
-#load the steps data
-steps = pd.read_csv('../steps_count.csv')
-steps=np.array(steps)
-print (steps.shape)
-#create a matrix of just steps data, removing the IDs
-y=steps[2:28,2:25]
-y=np.reshape(y,(y.shape[0]*y.shape[1],1))
+# Normalization
+X = df[["max", "min", "humidity"]].values #28 days data, 23 unique user id steps
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+y = df["steps"].values
+y = y.reshape(y.shape[0],1)
+
+print(X.shape)  # (644, 3)
+print(y.shape) # (644, 1)
+
+ones_x = np.ones((28*23, 1))
+X_ = np.concatenate((ones_x, X), axis = 1) # (644, 4)
+
+y = np.reshape(y,(y.shape[0]*y.shape[1], 1))
 y = np.asfarray(y, float)
 #define learning rate
 alpha = 0.0001
@@ -33,19 +28,25 @@ iters = 1000
 theta = np.array([[1.0,1.0,1.0,1.0]])
 
 #compute the mean squared error
-def computecost(X,y,theta):
-    inner = np.power((np.matmul(X,theta.T)-y),2)
-    return np.sum(inner)/(2*len(X))
-
-result = computecost(X_,y,theta)
-print(result)
+def computecost(X, y, theta):
+    inner = np.power((np.matmul(X, theta.T) - y), 2)
+    return np.sum(inner)/ (2* len(X))
 
 #compute the gradient and update the cost
-def gradientDescent(X,y,theta,alpha,iters):
+def gradientDescent(X, y, theta, alpha, iters):
     for i  in range(iters):
-        theta = theta - (alpha/len(X))*np.sum((X@theta.T-y)*X,axis=0)
-        cost = computecost(X,y,theta)
-    return (theta,cost)
+        theta = theta - (alpha/len(X)) *np.sum((X@theta.T-y) *X, axis = 0)
+        cost = computecost(X, y, theta)
+    return (theta, cost)
 
-g,cost=gradientDescent(X_,y,theta,alpha,iters)
-print(g,cost)
+g,cost = gradientDescent(X_, y, theta, alpha, iters)
+print(g, cost)
+# g contains W0, W1, W2 and W3 in order
+
+X1 = df[["max"]].values
+X2 = df[["min"]].values
+X3 = df[["humidity"]].values
+
+### Steps = W0 + W1* Max + W2 * Min + W3 * Humidity
+#y_pred = 343.31046642 + 12.71992071*X1 + 30.79605817*X2 -20.79486607*X3
+#print("Max step:", y_pred.max(), ", Min step: ", y_pred.min())
